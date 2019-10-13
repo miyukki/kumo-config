a7aa265098c384bbc97767b4b709df8005b9d833
Yusei Yamanaka <yusei1128@gmail.com>
2019-10-13 21:29:28 +0900

    show progress bar

--- kumo.js
+++ kumo.js
@@ -4,45 +4,57 @@ var app = new Vue({
     config: '',
     address: "",
     loading: false,
+
+    cueCount: 0,
+    successCueCount: 0,
+  },
+  computed: {
+    progress: function () {
+      return Math.round((this.successCueCount / this.cueCount) * 100)
+    },
   },
   methods: {
-    apply: function(event) {
+    apply: function () {
+      this.successCueCount = 0;
+      this.loading = true;
+
       let config = JSON.parse(this.config);
       let promises = [].concat(
-        config.sources.reduce(function(ps, item) {
-          return ps.concat(setSourceConfig(this.address, item))
-        }.bind(this), []),
-        config.destinations.reduce(function(ps, item) {
-          return ps.concat(setDestinationConfig(this.address, item))
-        }.bind(this), []),
-      )
-      Promise.all(promises).then(function() {
+        config.sources.flatMap(function (item) { return setSourceConfig(this.address, item) }.bind(this)),
+        config.destinations.flatMap(function (item) { return setDestinationConfig(this.address, item) }.bind(this)),
+      ).map(function (promise) {
+        return promise.then(function () {
+          return this.successCueCount++;
+        }.bind(this))
+      }.bind(this));
+      this.cueCount = promises.length;
+
+      Promise.all(promises).then(function () {
         this.loading = false;
         alert("success!");
-      }.bind(this)).catch(function(err) {
+      }.bind(this)).catch(function (err) {
         this.loading = false;
         alert("fail!");
       }.bind(this));
-      this.loading = true;
     },
   }
 });
 
 function setSourceConfig(address, item) {
-  let colorId = Math.floor((item.id-1) / 16) * 32 + ((item.id-1) % 16) + 1;
-  return Promise.all([
+  let colorId = Math.floor((item.id - 1) / 16) * 32 + ((item.id - 1) % 16) + 1;
+  return [
     // set line1
     fetch("http://" + address + "/config?action=set&configid=0&paramid=eParamID_XPT_Source" + item.id + "_Line_1&value=" + item.label1),
     // set line2
     fetch("http://" + address + "/config?action=set&configid=0&paramid=eParamID_XPT_Source" + item.id + "_Line_2&value=" + item.label2),
     // set color
     fetch("http://" + address + "/config?action=set&configid=0&paramid=eParamID_Button_Settings_" + colorId + "&value=" + JSON.stringify({ classes: "color_" + item.color }).replace(/[\"]/g, '\\\"')),
-  ]);
+  ];
 }
 
 function setDestinationConfig(address, item) {
-  let colorId = 16 + Math.floor((item.id-1) / 16) * 32 + ((item.id-1) % 16) + 1;
-  return Promise.all([
+  let colorId = 16 + Math.floor((item.id - 1) / 16) * 32 + ((item.id - 1) % 16) + 1;
+  return [
     // set line1
     fetch("http://" + address + "/config?action=set&configid=0&paramid=eParamID_XPT_Destination" + item.id + "_Line_1&value=" + item.label1),
     // set line2
@@ -53,5 +65,5 @@ function setDestinationConfig(address, item) {
     fetch("http://" + address + "/config?action=set&configid=0&paramid=eParamID_XPT_Destination" + item.id + "_Locked&value=" + (item.lock ? "1" : "0")),
     // set source
     fetch("http://" + address + "/config?action=set&configid=0&paramid=eParamID_XPT_Destination" + item.id + "_Status&value=" + item.source),
-  ]);
+  ];
 }
